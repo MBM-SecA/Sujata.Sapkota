9ce769572c30c9c436107028e844728f243def0e
Sujata.Sapkota <sujatasapkota4@gmail.com>
2020-09-26 22:07:51 +0545

    LinqWork update

--- LINQ/Country.cs
+++ LINQ/Country.cs
@@ -26,10 +26,25 @@ public class Country
             Country c3 = new Country( "Germany", "Europe", 35637444, 5373333);
             Country c4 = new Country( "USA", "North America", 124245.55, 3346474);
             Country c5 = new Country( "Russia", "Europe", 124245.55, 3346474);
-            Country c6 = new Country( "Sweden", "Europe", 124245.55, 3346474);
+            Country c6= new Country( "Austria", "Europe", 124245.55, 3346474);
+            Country c7= new Country( "Korea", "Asia", 124245.55, 3346474);
+            Country c8= new Country( "Croatia", "Europe", 124245.55, 3346474);
+            Country c9 = new Country( "Norway", "Europe", 124245.55, 33474);
+            Country c10= new Country( "Germany", "Europe", 124245.55, 3346474);
+            Country c11= new Country( "Finland", "Europe", 124245.55, 3346474);
+            Country c12= new Country( "Poland", "Europe", 2121212.121, 134);
+            Country c13= new Country( "Italy", "Europe", 124245.55, 3346474);
+            Country c14= new Country( "Uk", "Europe", 124245.55, 3346474);
+            Country c15 = new Country( "Sweden", "Europe", 124245.55, 33474);
+            Country c16= new Country( "Ukraine", "Europe", 124245.55, 6475664);
+            Country c17 = new Country( "France", "Europe", 124245.55, 7874);
+            Country c18 = new Country( "Spain", "Europe", 10000, 33475858);
+            Country c19= new Country( "Romania", "Europe", 124245.55, 3346474);
+            Country c20= new Country( "Greece", "Europe", 124245.55, 474);
+           
 
 
-            var countries = new List<Country> {c1,c2,c3,c4,c5,c6};
+            var countries = new List<Country> {c1, c2, c3, c4, c5, c6, c7, c8, c9, c10,c11,c12,c13,c14,c15,c16,c17,c18,c19,c20};
 
             return countries; 
     }
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-
+ 
 namespace LINQ
 {
     class Program
@@ -10,28 +10,25 @@ namespace LINQ
         {
             //LINQ - Language Integrated Query
             //LINQ to objects
-            
-            int[] numbers = { 23, 34, 56, 12, 23, 89, 67, 10, 445, 32, 66, 69 };
+            int[] numbers = { 23, 34, 56, 12, 23, 89, 65, 10, 44, 33, 66, 69 };
             //fetch numbers which are greater than 50
             var result1 = numbers.Where(x => x > 50 && x < 70);
             //Using Sql query syntax - query expression
             var result2 = from x in numbers where x > 50 && x < 70 select x;
-
-            string[] names = { "Suza", "Ram", "Luffy", "Zoro", "Ronaldo", "royal"};
+ 
+            string[] names = { "Suza", "Ram", "Luffy", "Zoro", "Ronaldo", "Robbon"};
             var result3 = names.Where(y => y.Length > 3 && y.StartsWith("R"));
             var result4 = from y in names where y.Length > 3 && y.ToLower().StartsWith("r") select y;
-            foreach(var name in result4)
-            {
-                Console.WriteLine(name);
-            }
-
-
+ 
             var countries = Country.GetCountries();
-            var asianCountries = from country in countries where country.Continent == "Asia" select country;
-
+            var EuCountries = from country in countries where country.Continent == "Europe" && country.Population  < 100000 select country.Name;
+            foreach(var country in EuCountries)
+            {
+                Console.WriteLine(country);
+            }
+ 
 //List countries in Europe which has population less than 100k.
 //List countries in Asia which are not ever invaded.
-
         }
     }
 }
